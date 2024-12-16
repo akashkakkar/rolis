d9294726d8eb99a22795bcb00aa928317b07a95a
Akash Kakkar <akash.kakkar@gmail.com>
2024-12-17 03:23:24 +0530

    Uploading assets v2

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // other configurations...
+  images: {
+    unoptimized: true,
+  }
 }
 
 module.exports = nextConfig
-
