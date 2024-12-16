217ea43ba653944495618b94523c4463b51b4b40
Akash Kakkar <akash.kakkar@gmail.com>
2024-12-17 02:58:49 +0530

    Fix TypeScript errors and add build cache v5

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,8 +1,11 @@
 import * as React from "react"
 import { cn } from "@/lib/utils"
 
-export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  // Add at least one custom prop to avoid the empty interface warning
+  // or keep existing HTML input props
+  className?: string;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
@@ -22,4 +25,3 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
 Input.displayName = "Input"
 
 export { Input }
-
