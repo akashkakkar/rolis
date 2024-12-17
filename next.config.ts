57e2b4f5f9df0a277be95bc185c7427adac99fd2
Akash Kakkar <akash.kakkar@gmail.com>
2024-12-17 11:58:42 +0530

    Uploading assets v9

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  basePath: '/rolis',
-  images: {
-    unoptimized: true
-  }
+ output: 'export',
+ basePath: '/rolis',
+ images: {
+   unoptimized: true
+ }
 }
-
-module.exports = nextConfig
-
-
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   basePath: '/rolis',
-//   images: {
-//     unoptimized: true
-//   }
-// }
-
-// module.exports = nextConfig
