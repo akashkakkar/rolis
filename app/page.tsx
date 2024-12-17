9f572068cc0e2f39ed5c77eec837f094c42f7355
Akash Kakkar <akash.kakkar@gmail.com>
2024-12-18 02:16:52 +0530

    form submission lambda v10

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { Hero } from '@/components/hero'
 import { Services } from '@/components/services'
 import { About } from '@/components/about'
-import { Contact } from '@/components/contact'
+import Contact from '@/components/contact'
 import { Gallery } from '@/components/gallery'
 
 export default function Home() {
