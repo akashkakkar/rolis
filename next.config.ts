e9e45f8057968db67cb44f9afc3798ef27395110
Akash Kakkar <akash.kakkar@gmail.com>
2024-12-17 12:49:25 +0530

    Uploading assets v11

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,20 @@
 const nextConfig = {
  output: 'export',
  basePath: '/rolis',
+ assetPrefix: '/rolis/',
  images: {
    unoptimized: true
  }
 }
+
+module.exports = nextConfig
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//  output: 'export',
+//  basePath: '/rolis',
+//  images: {
+//    unoptimized: true
+//  }
+// }
